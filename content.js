b998083f0061fd40b9f9483108e03ae2829bf526
lavedon <lavedon@gmail.com>
2019-05-07 20:21:22 -0400

    company name works.

--- content.js
+++ content.js
@@ -19,55 +19,32 @@ function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-scrape_results();
 
-function scrape_results() {
+async function scrape_results() {
 for (let i = 0; i < blocks.length; i++) {
+    try {
     console.log("Scraping block # " + i);
     let tempCompanyName = blocks[i].getElementsByTagName("a")[1].innerText;
-    let tempDescription = blocks[i].getElementsByTagName("p")[1].innerText;
-    let tempWebAddressHTML = blocks[i].getElementsByTagName("a")[6].parentNode.innerHTML;
-    let resultWeb = /"(.*?)"/.exec(tempWebAddressHTML);
-    let url = resultWeb[1];
-    let tempEmailContact = blocks[i].getElementsByTagName("a")[10].innerText;
-    let tempMinimumProjectSize = blocks[i].getElementsByClassName("list-item")[0].innerText;
-    let tempNumOfEmployees = blocks[i].getElementsByClassName("list-item")[2].innerText;
-    let tempCity = blocks[i].getElementsByClassName("list-item")[3].innerText;
-    let tempClutchProfileHTML = blocks[i].getElementsByTagName("li")[2].innerHTML;
-    let resultProfile = /"(.*?)"/.exec(tempWebAddressHTML);
-    let tempProfileUrl = resultProfile[1];
-    let tempPhoneNumber = blocks[i].getElementsByTagName("li")[3].getElementsByClassName("item __color6a")[0].innerText;
-    let chartText = blocks[1].getElementsByClassName("chartAreaContainer")[0].innerHTML;
-    let chartArray = chartText.match(/data-content=".*?"/gm);
-//     let chartDataDirty = /data-content="(.*?)"/gm.(chartText);
-    specialities = "";
-    for (let y = 0; y < chartArray.length; y++) {
-        // Positive Lookbehind (?<=<i>)
-        let newRatio = /(?<=<i>).+?(?=<\/i>)/.exec(chartArray[y]);
-        // newRatio[0] now equals the ratio we want
-        let newSpecialty = /(?<=<b>).+?(?=<\/b>)/.exec(chartArray[y]);
-        // newSpecialty[0] now equals the specialty we want
-        specialities = specialities + newSpecialty[0] + ": " + newRatio[0] + ".  ";
+    let companyName = "&co=" + tempCompanyName.replace(/&/g, "and");
+
+    let tempTagLine = blocks[i].getElementsByClassName("tagline");
+    
+
+    output = encodeURI(webappUrl+"?"+companyName);
+    await sleep(1000);
+        
+    console.log("output is " + output);
+    } catch(e) {
+    console.log(e);
     }
 
-    companyName = "&co=" + tempCompanyName.replace(/&/g, "and");
-    description = "&des=" + tempDescription.replace(/&/g, "and");
-    description = description.replace(/\"/g, "");
-    phoneNumber = "&phone=" + tempPhoneNumber;
-    numOfEmployees = "&emp=" + tempNumOfEmployees;
-    city = "&cs=" + tempCity;
-    profileUrl = "&pro=" + tempProfileUrl;
-    specialties = "&spe=" + specialties;
-
-    output = encodeURI(webappUrl+"?"+companyName+description+phoneNumber+numOfEmployees+city+profileUrl);
-    console.log("output is " + output);
 	try {
 		chrome.runtime.sendMessage({ message: output });
 		} catch(err) {
 		console.log(err);
 		}
-
-
 	}
 }
+scrape_results();
+
 setTimeout(function(){document.body.style = "background: #fff";}, 500);
